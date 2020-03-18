77b2bf771d8d806e9e7b1162914468ec5f491c5d
= <macroldj@163.com>
2020-03-18 23:24:59 +0800

    init flask APi

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-### 
+### Flask Api
+### Refence
+- [Flask Restful Api](https://flask-restful.readthedocs.io/en/latest/)
+- [flask docs](https://flask.palletsprojects.com/en/1.1.x/)
---
+++ urls.py
@@ -0,0 +1,8 @@
+from flask_restful import Api
+
+api = Api(app)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
