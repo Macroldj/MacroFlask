88a54b810510671f9ce46c50cb33ae12ed5b69bb
Li, DejinX <dejinx.li@intel.com>
2020-08-04 13:40:07 +0800

    flask feature function

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
-from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
 
 from utils.app import create_app
 from utils.config import Config
 
 app = create_app(Config)
 manager = Manager(app=app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 
 if __name__ == '__main__':
     manager.run()
