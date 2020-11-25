593a09ac10af88664f436713eb242366d135bdef
Durgin <macroldj@163.com>
2020-11-25 23:22:17 +0800

    rabbitMQ init

--- ulits/__init__.py
+++ ulits/__init__.py
@@ -5,5 +5,3 @@ from .cache import RedisCache
 
 redisPool = redis.ConnectionPool(host=config.REDIS_HOST, port=config.CACHE_REDIS_PORT, decode_responses=True, max_connections=config.CACHE_REDIS_MAX_CONN)
 myCache = RedisCache(connection_pool=redisPool)
-
-# MessageQueuePool = pika.BlockingConnection(pika.ConnectionParameters(config.MQ_HOST))
